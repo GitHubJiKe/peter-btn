13e777eba5941c8abb515b144a1e2cbeb662cc34
peter.yuan <pengfei.yuan@boldseas.com>
2019-10-23 18:15:55 +0800

    first commit

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,22 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
+import React, { Component } from "react";
+import { render } from "react-dom";
 
-import Example from '../../src'
+import Example from "../../src";
 
 class Demo extends Component {
   render() {
-    return <div>
-      <h1>peter-btn Demo</h1>
-      <Example/>
-    </div>
+    return (
+      <div>
+        <h1>peter-btn Demo</h1>
+        <Example
+          text="Button"
+          onClick={() => {
+            alert("fuck youuuu");
+          }}
+        />
+      </div>
+    );
   }
 }
 
-render(<Demo/>, document.querySelector('#demo'))
+render(<Demo />, document.querySelector("#demo"));
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-import React, {Component} from 'react'
-
+import React, { Component } from "react";
+import "./style.css";
 export default class extends Component {
+  onClick = () => {
+    console.log("default click function");
+  };
+
   render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
+    const { text, onClick } = this.props;
+    const _click = onClick || this.onClick;
+    return (
+      <button className="btn" onClick={_click}>
+        {text}
+      </button>
+    );
   }
 }
